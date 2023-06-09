12ea0748528625bead1104b85dfad222d56e299a
linm1 <linm1@bxscience.edu>
2023-06-09 17:51:57 -0400

    changed meta data

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,7 +8,8 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <title>Next.js</title>
+        <title>TikTok Favorites</title>
+        <link rel="icon" href="/favicon.ico" />
       </head>
       <body>
         {children}
