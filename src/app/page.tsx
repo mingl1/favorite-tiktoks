9d903e71b60bf17d97b9028eafd3f119d787643b
mingl1 <linm1@bxscience.edu>
2024-01-07 14:13:21 -0500

    cs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -37,7 +37,7 @@ const Home: NextPage = () => {
       const coldStart = setTimeout(() => {
         setColdStart(true);
       }, 1000);
-      await fetch(`/api/tt/coldStart`).then((_) => {
+      await fetch(`/api/coldStart`).then((_) => {
         setColdStart(false);
         clearTimeout(coldStart);
       });
