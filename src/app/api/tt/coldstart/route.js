89cbcb2189d6639ead71b959cf6a2c0d3ac4d392
mingl1 <linm1@bxscience.edu>
2024-01-07 14:09:17 -0500

    ts

--- src/app/api/tt/coldstart/route.js
+++ src/app/api/tt/coldstart/route.js
@@ -1,6 +1,10 @@
 import { NextResponse } from "next/server";
 import coldStart from "../../../../defer/coldStart";
 
+/**
+ * @param {any} _request
+ * @returns {Promise<NextResponse>}
+ */
 export async function GET(_request) {
   await coldStart();
   return NextResponse.json({ coldStart: "done" });
