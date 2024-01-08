a3e938cb7f94fd952f04ffc710e4ada6be84f8ce
mingl1 <linm1@bxscience.edu>
2024-01-08 00:53:53 -0500

    idk

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,20 +1,25 @@
 import { Analytics } from "@vercel/analytics/react";
 import "../styles/globals.css";
+import { QueryClient, QueryClientProvider } from "react-query";
+const queryClient = new QueryClient();
+
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <head>
-        <title>TikTok Favorites</title>
-        <link rel="icon" href="/favicon.ico" />
-      </head>
-      <body className="bg-[#15162c]">
-        {children}
-        <Analytics />
-      </body>
-    </html>
+    <QueryClientProvider client={queryClient}>
+      <html lang="en">
+        <head>
+          <title>TikTok Favorites</title>
+          <link rel="icon" href="/favicon.ico" />
+        </head>
+        <body className="bg-[#15162c]">
+          {children}
+          <Analytics />
+        </body>
+      </html>
+    </QueryClientProvider>
   );
 }
