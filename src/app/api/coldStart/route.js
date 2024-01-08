588503e335fd73ccf7abbd1bbbed1248ba66fef8
mingl1 <linm1@bxscience.edu>
2024-01-07 21:14:01 -0500

    a

--- src/app/api/coldStart/route.js
+++ src/app/api/coldStart/route.js
@@ -1,5 +1,6 @@
 import { NextResponse } from "next/server";
 import coldStart from "../../defer/coldStart";
+// import dynamic from "next/dynamic";
 
 /**
  * @param {any} _request
@@ -9,3 +10,4 @@ export async function GET(_request) {
   await coldStart();
   return NextResponse.json({ coldStart: "done" });
 }
+export const dynamic = "force-dynamic";
