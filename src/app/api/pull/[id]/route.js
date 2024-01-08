c59752caa0a5c4916b91fdfd68d234b74ff81e8f
mingl1 <linm1@bxscience.edu>
2024-01-08 00:46:11 -0500

    ts

--- src/app/api/pull/[id]/route.js
+++ src/app/api/pull/[id]/route.js
@@ -1,3 +1,11 @@
+import { getExecution } from "@defer/client";
+/**
+ * @param {any} _request
+ * @param {{ params: { id: string } }} param1
+ * @returns {Promise<NextResponse>}
+ */
 export async function GET(_request, { params }) {
   const { id } = params;
+  const execution = await getExecution(id);
+  return NextResponse.json(execution);
 }
